917807d1c5d187e53777565719dec660946cb270
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-09-30 19:38:28 +0200

    Add FetchIssues class to fetch issues from a repository and label

---
+++ app/models/github/fetch_issues.rb
@@ -0,0 +1,36 @@
+module Github
+  class FetchIssues
+    def self.call(repository:, label:)
+      new(repository: repository, label: label).fetch!
+    end
+
+    def initialize(repository:, label:)
+      @repository = repository
+      @label = label
+    end
+
+    def fetch!
+      JSON.parse(response.body)
+    end
+
+    private
+
+    def response
+      @response ||= fetch_response!
+    end
+
+    def fetch_response!
+      RestClient.get(issues_uri, params: labels_params)
+    end
+
+    def issues_uri
+      "https://api.github.com/repos/#{repository}/issues"
+    end
+
+    def labels_params
+      { labels: label }
+    end
+
+    attr_reader :repository, :label
+  end
+end
