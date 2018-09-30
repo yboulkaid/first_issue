553c775bec99f4e01a4e578d26d70a6fb849093a
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-09-30 21:36:54 +0200

    Add milligram CSS framework

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module FirstIssue
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('node_modules')
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
