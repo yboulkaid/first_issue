ce64b7ce0b1f50ea4904a15e7f707bdeec041b60
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-09-30 22:38:03 +0200

    Add more repositories

--- app/models/github/repositories.rb
+++ app/models/github/repositories.rb
@@ -2,7 +2,10 @@ module Github
   class Repositories
     REPOSITORIES = [
       Repository.new(name: 'babel/babel', language: :javascript, first_issue_label: 'good first issue'),
-      Repository.new(name: 'rails/rails', language: :ruby, first_issue_label: 'good-first-patch')
+      Repository.new(name: 'webpack/webpack', language: :javascript, first_issue_label: 'D0: My First Commit (Contrib. Difficulty)'),
+      Repository.new(name: 'rails/rails', language: :ruby, first_issue_label: 'good-first-patch'),
+      Repository.new(name: 'rails/webpacker', language: :ruby, first_issue_label: 'good first issue'),
+      Repository.new(name: 'rubocop-hq/rubocop', language: :ruby, first_issue_label: 'good first issue')
     ].freeze
 
     def self.all_by_language
